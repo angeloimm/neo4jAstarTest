dbf806f8cbce951dde06c9cb60e2d2e2b5a58a91
sunrelax <gfcinque@inwind.it>
2016-09-22 16:20:35 +0200

    test

---
+++ 1.sql
@@ -0,0 +1,4 @@
+http://www.sanita.puglia.it//ServizioAlbopretorioWeb/ricercaItem.ctrl?aziendaParam=asllecce&page=2&paginazione=true&tipoItemParam=bando
+
+
+http://www.sanita.puglia.it//ServizioAlbopretorioWeb/registroItem.ctrl;jsessionid=18E260AA62DBA6A3112245AA48F9C944.psnode2?aziendaParam=asltaranto&page=2&paginazione=true
